f67ea5803132375cbe5da721d8e1be9f2a6cafd8
mingjun.fan@edaili.com <mingjun.fan@edaili.com>
2021-12-15 18:07:07 +0800

    init

---
+++ src/test/java/com/moon/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
